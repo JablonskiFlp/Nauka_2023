303010b5b2436e86ad81d27c1d7054d9d8f9a3d2
filip <KSAKL@gmail.com>
2023-11-23 12:07:08 +0100

    New section - gifts

---
+++ PHP/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>cinemacityhtml</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Filip Jablonski">
+    <meta name="keywords" content="slowakluczowe"> 
+    <meta name="description" content="opis">
+    <!-- <link rel = "stylesheet" href = "style_nauka.css" >   -->
+    <link href="Icons/bot_smile_robot_robo_chatbot_assistant_advisor_icon_256856.ico" type="image/x-icon" rel="shortcut icon">
+    <link rel = "stylesheet" href = "../CSS/header.css" >  
+    <link rel = "stylesheet" href = "../CSS/footer.css" >  
+    <link rel = "stylesheet" href = "../CSS/index.css" >  
+    <link rel = "stylesheet" href = "../CSS/three_photos_article.css" >  
+    <link rel = "stylesheet" href = "../CSS/toolbar.css" >  
+    <link rel = "stylesheet" href = "../CSS/mini_blog.css" >  
+</head>
+<body>
+    <?php include "../HTML/header.html" ?>
+
+    <article class="movieposter_1">
+        <a target="_blank" href="http://127.0.0.1:5500/cinema-city.html#" >
+            <img src="../IMAGES/posters/movieposter_1.jpg" alt="ourLogo" > 
+        </a>
+    </article>
+    <main>
+        <section>
+            <div class="container">
+                <p class="loremi">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa, libero! Similique odit ullam laudantium! Minima nobis impedit, assumenda ipsum consequatur quis odio porro sed ipsa tempora harum in labore fugit.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt aliquid maxime nisi ut, blanditiis alias ex exercitationem eligendi. Ex quas, sint laborum nesciunt aspernatur temporibus omnis. Optio nisi corrupti aut!
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil id illum ea cum quibusdam mollitia, pariatur laborum nulla a vero. Sunt laborum molestiae assumenda totam nobis voluptas atque ut nemo.
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore harum distinctio consectetur voluptatem molestiae quod iusto nostrum blanditiis beatae necessitatibus neque autem itaque atque tenetur, repudiandae aliquid tempore eius vel.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod labore porro placeat alias. Eius veritatis consequatur voluptatibus dolorem voluptates nemo dignissimos nostrum tempora suscipit expedita, laborum laboriosam saepe earum error.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, veniam at enim, sunt corrupti assumenda harum doloribus, iure perferendis molestias distinctio! Accusamus ut vitae saepe nisi atque vel voluptas sapiente.
+                </p>
+            </div>
+        </section>
+
+        <?php include "../HTML/three_photos_article.html" ?>
+        <?php include "../HTML/toolbar.html" ?>
+        <?php include "../HTML/mini_blog.html" ?>
+    </main>
+    <hr>
+    <?php include "../HTML/footer.html" ?>
+</body>
+</html>
---
+++ PHP/prezenty.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Blog - CinemaCity</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Filip Jablonski">
+    <meta name="keywords" content="slowakluczowe"> 
+    <meta name="description" content="opis">
+    <link rel = "stylesheet" href = "../CSS/prezenty.css" >  
+    <link rel = "stylesheet" href = "../CSS/header.css" >  
+    <link rel = "stylesheet" href = "../CSS/toolbar.css" >  
+    <link rel = "stylesheet" href = "../CSS/footer.css" >  
+</head>
+<body>
+    <?php include "../HTML/header.html" ?>
+    <?php include "../HTML/prezenty.html" ?>
+    <?php include "../HTML/toolbar.html" ?>
+    <?php include "../HTML/footer.html" ?>
+</body>
